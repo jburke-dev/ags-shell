6099a96c9278b3e0c47b7409370d343f8c7f93ce
Johan Burke <jburke.create@gmail.com>
2025-10-14 17:34:10 -0400

    docs: update readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ags-Shell
+
+Custom desktop shell, built with [AGS](https://github.com/Aylur/ags) and heavily inspired by [Matshell](https://github.com/Neurarian/matshell)
--- widgets/status-bar/clock.tsx
+++ widgets/status-bar/clock.tsx
@@ -5,7 +5,7 @@ import GLib from 'gi://GLib?version=2.0';
 export default function Clock({ format = '%H:%M - %a %b %e, %Y' }) {
     const time = createPoll('', 1000, () => {
         return GLib.DateTime.new_now_local().format(format)!;
-    });
+    }).as((t) => `\uf017 ${t}`);
     return (
         <menubutton>
             <label label={time} />
